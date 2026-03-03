90da63f3c43d1aa99a2fa40f96166cccfd4ab9e9
titetodesco <78027430+titetodesco@users.noreply.github.com>
2026-03-02 22:35:07 -0300

    Change database connection details in schema.sql
    Updated database connection URL and token in get_conn function.

--- schema.sql
+++ schema.sql
@@ -4,8 +4,8 @@ import libsql
 
 @st.cache_resource
 def get_conn():
-    url = st.secrets["TURSO_DATABASE_URL"]
-    token = st.secrets["TURSO_AUTH_TOKEN"]
+    url = st.secrets["libsql://sorg-titetodesco.aws-us-east-1.turso.io"]
+    token = st.secrets["eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzI0OTkzNTgsImlkIjoiMDE5Y2IxMzEtNzkwMS03YWIwLTg1YjgtMDQ5NzFlZjE0ZDQ5IiwicmlkIjoiNzUwZDdhZTMtYjNiMi00NGY4LWE1OGMtZmM5OGY5ZjAyMjY1In0.amsXjuhaHsN0BtITp5LTLsV2Gw-hwjcZkdREyc6ONVUJ9lYHbfUmG5lTCm0j4WaPTi-1ifss34iNgjKV2JdcBw"]
     conn = libsql.connect("sorg_macrobase.db", sync_url=url, auth_token=token)
     conn.sync()
     return conn
