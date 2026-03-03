020518071cd50d341815c2ea0d27194b9c26d57e
titetodesco <78027430+titetodesco@users.noreply.github.com>
2026-03-02 22:09:16 -0300

    Implement load_macrobase function for Excel sheets
    Added a function to load multiple sheets from an Excel file.

--- utils/excel_io.py
+++ utils/excel_io.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def load_macrobase(file):
+    return {
+        "EIXOS": pd.read_excel(file, sheet_name="EIXOS"),
+        "TEMAS": pd.read_excel(file, sheet_name="TEMAS"),
+        "TOPICOS": pd.read_excel(file, sheet_name="TOPICOS"),
+        "INDICADORES": pd.read_excel(file, sheet_name="INDICADORES"),
+        "VARIAVEIS": pd.read_excel(file, sheet_name="VARIAVEIS"),
+        "INDICADOR_VARIAVEL": pd.read_excel(file, sheet_name="INDICADOR_VARIAVEL"),
+    }
